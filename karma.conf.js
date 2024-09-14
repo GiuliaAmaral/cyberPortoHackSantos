3d5f6fa85e1cd452e0362a00c963f2becdc9445f
ecossistema.dev <contato@rubensflinco.com.br>
2024-09-14 19:07:17 -0300

    cyber-porto

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/cyber-punk'),
+      dir: require('path').join(__dirname, './coverage/cyber-porto'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cyber-punk'`, () => {
+  it(`should have as title 'cyber-porto'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cyber-punk');
+    expect(app.title).toEqual('cyber-porto');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('cyber-punk app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('cyber-porto app is running!');
   });
 });
