b6aae50a1c1be516d45affa214f28601e79d2bd5
Matheus Mastrocinque <tec.mastrocinque@gmail.com>
2024-09-15 05:36:48 -0300

    Integracao com Gemini API

--- src/app/components/card-navio/card-navio.component.ts
+++ src/app/components/card-navio/card-navio.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ModalComponent } from '../modal/modal.component';
 import { NaviosProgramados } from 'src/app/model/navios-programados.model';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-card-navio',
@@ -9,19 +10,43 @@ import { NaviosProgramados } from 'src/app/model/navios-programados.model';
   styleUrls: ['./card-navio.component.css'],
 })
 export class CardNavioComponent implements OnInit {
-  
-  @Input() navio?: NaviosProgramados;
 
+  @Input() navio?: NaviosProgramados;
+  textoGeminiAPI: string = '';
 
   constructor(
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    private apiService: ApiService,
   ) { }
 
   ngOnInit(): void {
   }
 
-  public openDialog(): void {
-    this.dialog.open(ModalComponent);
+  public openDialog(navio?: NaviosProgramados): void {
+    if (navio) {
+      this.dialog.open(ModalComponent);
+      this.construirPrompt(navio);
+      if (this.textoGeminiAPI) {
+        this.apiService.enviarDadosParaGemini(this.textoGeminiAPI)
+          .subscribe(
+            (response) => {
+              console.log(response);
+            },
+            (error) => {
+              console.error('Erro ao enviar dados para o Gemini:', error);
+            }
+          );
+      }
+    }
   }
 
+  public construirPrompt(navio: NaviosProgramados): string {
+    this.textoGeminiAPI = `Crie um relatório detalhado sobre o navio ${navio.nomenavio}, com número IMO ${navio.imo}. O navio está programado para atracar em ${navio.local} no dia ${navio.data}, entre ${navio.periodo}, transportando ${navio.mercadoria}.`;
+
+    // Adicione informações adicionais baseadas nos dados do navio
+    this.textoGeminiAPI += `* Data estimada de chegada (ETA): ${navio.eta}`;
+    this.textoGeminiAPI += `* Viagem: ${navio.viagem}` + 'Usar somente os dados que foram mencionados. E gerar algum tipo de estatistica da possibilidade dessa viagem dar certo';
+
+    return this.textoGeminiAPI;
+  }
 }
