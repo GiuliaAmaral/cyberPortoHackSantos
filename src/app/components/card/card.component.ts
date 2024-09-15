ca374907cc44b118e99ebda56e126cefd149f980
Giulia Amaral <contato.giuliaamaral@gmail.com>
2024-09-14 23:28:53 -0300

    subindo cards

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Atracagem } from 'src/app/model/atracagem.model';
 import { ApiService } from 'src/app/services/api.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalComponent } from '../modal/modal.component';
 
 @Component({
   selector: 'app-card',
@@ -10,13 +12,20 @@ import { ApiService } from 'src/app/services/api.service';
 export class CardComponent implements OnInit {
   atracagens: Atracagem[] = [];
 
-  constructor(private atracagemService: ApiService) { }
+  constructor(
+    private atracagemService: ApiService,
+    public dialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
     this.atracagemService.getAtracagens().subscribe(atracagens => {
       this.atracagens = atracagens;
       console.log(this.atracagens);
     });
+  }
+
+  public openDialog(): void {
+    this.dialog.open(ModalComponent);
   }
 
 }
